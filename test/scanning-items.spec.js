743ed1dcfd728c2d77ea1ba4e9284c33a31097af
mcannon <m.cannon.dev@gmail.com>
2019-01-24 14:57:29 +0000

    Use an alternative test language to surface the 'tell, don't ask' principle.

--- test/scanning-items.spec.js
+++ test/scanning-items.spec.js
@@ -4,5 +4,14 @@ describe("scanning items", () => {
     // This is an example of a "negative test" (Beck) and not recommended as a starting point.
     // TDD is not about *testing*, it's about evolving a *design*.
     // Negative tests don't move us forward. They don't give us an opportunity to consider design.
-    xit("gives a total of 0 when nothing scanned", () => {});
+    xit("gives a total cost of 0 when nothing is scanned", () => {});
+
+    it("gives a total cost of 50 when something is scanned", () => {
+        // Even with this first spec we have to consider a design that honours the domain ideas of "total",
+        // "cost", "scanning". The idea of "giving" steers our desing towards the "tell, don't ask" principle.
+    });
+
+    it("total cost should be 50 when something is scanned", () => {
+        // Using this language steers our design away from the "tell, don't ask" principle.
+    });
 });
